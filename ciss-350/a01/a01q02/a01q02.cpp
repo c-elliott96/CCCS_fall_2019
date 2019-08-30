637bd7bf801900442778feae1b0d23ac85537d27
Christian Elliott <cdelliott96@gmail.com>
2019-08-30 18:29:10 -0500

    a01q02 'rising hills' added, unfinished

---
+++ ciss-350/a01/a01q02/a01q02.cpp
@@ -0,0 +1,21 @@
+// Name: Christian Elliott
+// File: a01q02.cpp
+
+#include <iostream> 
+
+
+void draw_rising_hills(int n)
+{
+    // DO WORK HERE
+}
+
+
+int main()
+{
+    int n;
+    std::cin >> n;
+
+    draw_rising_hills(n);
+
+    return 0;
+}
