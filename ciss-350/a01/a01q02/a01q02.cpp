08df0366effbb8e664022d518abb6d7e4dfee1df
c-elliott96 <36754206+c-elliott96@users.noreply.github.com>
2019-09-06 13:25:57 +0000

    Began work on rising hills

--- ciss-350/a01/a01q02/a01q02.cpp
+++ ciss-350/a01/a01q02/a01q02.cpp
@@ -1,12 +1,29 @@
-// Name: Christian Elliott
 // File: a01q02.cpp
+// Name: Christian Elliott
 
-#include <iostream> 
+#include <iostream>
 
 
 void draw_rising_hills(int n)
 {
-    // DO WORK HERE
+    int n_counter = n;
+    for (int i = 1; i <= n; ++i)
+    {
+        for (int j = 1; j <= n_counter * (n_counter - 1); ++j)
+        {
+            std::cout << ' ';
+        }
+        std::cout << '*' << '\n';
+        
+        if (i > 2)
+        {
+            for (int j = 1; j <= i; ++j)
+            {
+                std::cout << 
+            }
+        }
+        --n_counter;
+    }
 }
 
 
@@ -16,6 +33,5 @@ int main()
     std::cin >> n;
 
     draw_rising_hills(n);
-
     return 0;
 }
