46252181ae38f2d6653becd3e3c379e07534e1ff
Christian Elliott <cdelliott96@gmail.com>
2019-09-10 21:02:28 +0000

    finished a01q02

--- ciss-350/a01/a01q02/a01q02.cpp
+++ ciss-350/a01/a01q02/a01q02.cpp
@@ -7,21 +7,37 @@
 void draw_rising_hills(int n)
 {
     int n_counter = n;
+    int star_count = 1;
+    int spaces = ((n - 1) * 2) - 2;
     for (int i = 1; i <= n; ++i)
     {
         for (int j = 1; j <= n_counter * (n_counter - 1); ++j)
         {
             std::cout << ' ';
         }
-        std::cout << '*' << '\n';
-        
-        if (i > 2)
+
+        for (int j = 1; j <= i; ++j)
         {
-            for (int j = 1; j <= i; ++j)
+            for (int k = 1; k <= star_count; ++k)
             {
-                std::cout << 
+                std::cout << '*';
             }
+
+            for (int k = 1; k <= spaces; ++k) // spaces after 1st star of each
+                                                        //  iteration of i
+            {
+                std::cout << ' ';
+            }
+
+            star_count += 2;
+        } 
+
+        star_count = 1;
+        if (i > 1)
+        {
+            spaces -= 2; 
         }
+        std::cout << '\n'; 
         --n_counter;
     }
 }
