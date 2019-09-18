0cf08fa30340ce37bc8e79124e72d40b303fbfcb
Christian Elliott <cdelliott96@gmail.com>
2019-09-17 20:11:52 -0500

    finished a01q04, also added a comment doc

--- ciss-350/a01/a01q04/a01q04.cpp
+++ ciss-350/a01/a01q04/a01q04.cpp
@@ -6,28 +6,60 @@
 
 void increment(int digits[], int len_digits, int base)
 {
-    
+  bool carry = false;
+  for (int i = len_digits - 1; i >= 0; --i)
+	{
+	  if (!carry && digits[i] == base - 1)
+		{
+		  carry = true;
+		  digits[i] = 0;
+		  continue;
+		}
+	  else if (carry && digits[i] != base - 1)
+		{
+		  carry = false;
+		  ++digits[i];
+		  return;
+		}
+	  else if (carry && digits[i] == base - 1)
+		{
+		  digits[i] = 0;
+		  continue;
+		}
+	  else
+		{
+		  ++digits[i];
+		  return;
+		}
+	}
 }
 
 
 void print(int digits[], int len_digits)
 {
-    for (int i = 0; i < len_digits; ++i)
-    {
-        std::cout << digits[i] << ',';
-    }
-    std::cout << std::endl;
+  /*
+  for (int i = len_digits - 1; i >= 1; --i)
+	{
+	  std::cout << digits[i] << ','; 
+	}
+  */
+
+  for (int i = 0; i < len_digits - 1; ++i)
+	{
+	  std::cout << digits[i] << ',';
+	}
+  std::cout << digits[len_digits - 1] << std::endl;
 }
 
 
 int main()
 {
-    int digits[4] = {0};
-    for (int i = 0; i < 100; ++i)
+  int digits[4] = {0};
+  for (int i = 0; i < 100; ++i)
     {
-        print(digits, 4);
-        increment(digits, 4, 16);
+	  print(digits, 4);
+	  increment(digits, 4, 16);
     }
  
-    return 0;
+  return 0;
 }
