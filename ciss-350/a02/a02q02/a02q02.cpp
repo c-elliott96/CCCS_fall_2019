ec1376a25279010b95a70e147a26a66ce8ba043e
Christian Elliott <cdelliott96@gmail.com>
2019-09-24 13:20:29 -0700

    Finalized, but not complete. Basic fxn working.

--- ciss-350/a02/a02q02/a02q02.cpp
+++ ciss-350/a02/a02q02/a02q02.cpp
@@ -4,25 +4,41 @@
 #include <iostream>
 
 
-int f(int n, int a=3, int b=1, int d=2)
+int f(int n, int a, int b, int d)
 {
   if (n % d == 0) // if n is divisible by d
 	{
-	  n = n / d;
+    return n / 2;
 	}
 
   else // if n is not divisible by d
 	{
-	  n = (a * n) + b;
+    return (3 * n) + 1;
 	}
-
-  return n;
 }
 
 
-int fs(int * & p, int n, int a=3, int b=1, int d=2)
+int fs(int * &p, int n, int N, int a, int b, int d)
 {
-  
+  int i = 0;
+  for (; i <= N; ++i)
+  {
+    if (i == 0)
+    {
+      p[i] = n;
+    }
+    else
+    {    
+      n = f(n, a, b, d);
+      p[i] = n;
+      if (n == 1)
+      {
+        p[i] = n;
+        return i;
+      }
+    }
+  }
+  return i + 1;
 }
 
 
@@ -30,8 +46,32 @@ int main()
 {
   int a, b, d, n, N;
   std::cin >> a >> b >> d >> n >> N;
-  
-  // run while loop here that runs f and puts return value into arr[]
+  int * p = new int[N + 1];
+  /*
+  for (int i = 0; i < N; ++i)
+  {
+    p[i] = new int;
+  } 
+  */
+
+  int i = fs(p, n, N, a, b, d);
+
+  std::cout << a << ' ' << b << ' ' << d << ' ' << n << ' ' << N << ' ';
+  if (p[i] == 1) 
+  {
+    std::cout << "pass ";
+  }
+  else 
+  {
+    std::cout << "fail ";
+  }
+  std::cout << i << ' ';
+  for (int j = 0; j < i; ++j)
+  {
+    std::cout << p[j] << ' ';
+  }
+  std::cout << std::endl;
+  delete [] p;
   
   return 0;
 }
