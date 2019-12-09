629e82cfe6723ac8c7a27c5e3b49b0e8234eb0a9
Christian Elliott <cdelliott96@gmail.com>
2019-12-08 22:51:14 -0600

    idk what I'm committing with this one

--- quadtree/main.cpp
+++ quadtree/main.cpp
@@ -1,36 +1,5 @@
 #include <iostream>
 #include <vector>
 
-
-class Objects
-{
-public:
-  int x;
-  int y;
-  int dx;
-  int xy;
-}
-
-  
-class Node
-{
-public:
-  Node * parent_;
-  int maxx;
-  int maxy;
-  int minx;
-  int miny;
-  int t;        // threshold
-  int maxd;     // max depth --> this avoids infinite loop issues w/ recursion
-  std::vector< Object * > objs;
-  
-private:
-}
-
-
-int main()
-{
-  std::cout << "Hello World\n";
-
-  return 0;
-}
+/* Notes: Node is our only necessary structure class
+calling node.insert(particle) will 
