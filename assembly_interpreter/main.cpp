604121faf8d7d4dab78aa2ebab9faad2a677dc69
Christian Elliott <cdelliott96@gmail.com>
2019-12-11 15:38:05 -0600

    general initialization is complete - make decision tree now

--- assembly_interpreter/main.cpp
+++ assembly_interpreter/main.cpp
@@ -111,23 +111,11 @@ std::vector < unsigned int > interpret(std::vector < unsigned int > x)
 }
 
 
-void create_registers(std::map < int, unsigned int > & map)
+void create_registers(std::map < unsigned int, unsigned int > & map)
 {
-  for (int i = 0; i < 32; ++i)
+  for (unsigned int i = 0; i < 32; ++i)
     {
-        map.insert(std::pair< int , unsigned int > (i , 0));
-    }
-}
-
-
-void print_registers(std::map < int, unsigned int > map)
-{
-  std::map < int, unsigned int > :: iterator it = map.begin();
-  std::cout << "Registers: " << '\n';
-  while (it != map.end())
-    {
-      std::cout << '\t' << it->first << '\t' << it->second << '\n';
-      ++it;
+        map.insert(std::pair< unsigned int , unsigned int > (i , 0));
     }
 }
 
@@ -152,52 +140,53 @@ void print_instructions_uint(std::list < std::vector < unsigned int > > instruct
 
 
 /* ============ HASHES FOR ALL POSSIBLE EXPECTED COMMANDS =================
-add	297    597
-addi	402    1017    
-addiu   519    1602
-addu	414    1065
-and	307    617
-andi	412    1037
-beq	312    639
-bgez	424    1095
-bgezal	629    2228
-bgtz	439    1140
-blez	429    1105
-bltz	444    1150    slti AND noop conflict
-bltzal	649    2283
-bne	309    621
-div	323    664    
-divu	440    1132
-j	106    106
-jal	311    624
-jr	220    334
-lb	206    304
-lui	330    657    ori conflicts
-lw	227    346
-mfhi	420    1045
-mflo	430    1081
-mult	450    1131
-multu	567    1716
-noop	444    1113    slti AND bltz conflict
-or	225    339
-ori	330    654    lui conflicts
-sb	213    311
-sll	331    655
-sllv	449    1127
-slt	339    679
-slti	444    1099    bltz AND noop conflict
-sltiu	561    1684
-sltu	456    1147
-sra	326    634
-srl	337    667
-srlv	455    1139
-sub	330    643     ori AND lui conflict
-subu	447    1111
-sw	234    353
-syscall	763    2987
-xor	345    684
-xori	450    1104
-*/
+{
+  add	    297    597
+  addi	  402    1017    
+  addiu   519    1602
+  addu    414    1065
+  and     307    617
+  andi    412    1037
+  beq     312    639
+  bgez    424    1095
+  bgezal	629    2228
+  bgtz	  439    1140
+  blez	  429    1105
+  bltz	  444    1150    slti AND noop conflict
+  bltzal	649    2283
+  bne	    309    621
+  div	    323    664    
+  divu	  440    1132
+  j	      106    106
+  jal	    311    624
+  jr	    220    334
+  lb	    206    304
+  lui	    330    657    ori conflicts
+  lw	    227    346
+  mfhi	  420    1045
+  mflo	  430    1081
+  mult	  450    1131
+  multu	  567    1716
+  noop	  444    1113    slti AND bltz conflict
+  or	    225    339
+  ori	    330    654    lui conflicts
+  sb	    213    311
+  sll	    331    655
+  sllv	  449    1127
+  slt	    339    679
+  slti	  444    1099    bltz AND noop conflict
+  sltiu	  561    1684
+  sltu	  456    1147
+  sra	    326    634
+  srl	    337    667
+  srlv	  455    1139
+  sub	    330    643     ori AND lui conflict
+  subu	  447    1111
+  sw	    234    353
+  syscall	763    2987
+  xor	    345    684
+  xori	  450    1104
+}*/
 
 
 unsigned int alt_hash (std::string str)
@@ -216,189 +205,234 @@ unsigned int alt_hash (std::string str)
     {
       sum += ints[j];
     }
-  std::cout << sum << '\n';
+  //std::cout << sum << '\n';
   return sum;
 }
 
 		  
-// void do_action(std::vector < unsigned int > command)
-// {
-//   std::vector < unsigned int > pseudo;
-//   pseudo.push_back(interpret(command));
-//   unsigned int x = hash(pseudo);
-//   switch (x)
-//     {
-      
-//     }
-// }
+void do_action(std::vector < unsigned int > command)
+{
+  
+}
 
 
-void map_commands()
+std::map < unsigned int, std::string > map_commands()
 {
   std::list < std::string > list;
   std::string a = "add";
-  list.push_back(a);
-  // std::list < std::string > ::iterator it;
-  // for (it = list.begin(); it != list.end(); ++it)
-  //   {
-  //     std::cout << *(it) << '\n';
-  //   }
-  a.clear();
-  a = "addi";
-  list.push_back(a);
-  a.clear();
-  a = "addiu";
-  list.push_back(a);
-  a.clear();
-  a = "addu";
-  list.push_back(a);
-  a.clear();
+  list.push_back(a); 
 
-  a = "and";
-  list.push_back(a);
-  a.clear();
-  a = "andi";
-  list.push_back(a);
-  a.clear();
-  a = "beq";
-  list.push_back(a);
-  a.clear();
+  // create strings to hash
+  {
+    a.clear();
+    a = "addi";
+    list.push_back(a);
+    a.clear();
+    a = "addiu";
+    list.push_back(a);
+    a.clear();
+    a = "addu";
+    list.push_back(a);
+    a.clear();
 
-  a = "bgez";
-  list.push_back(a);
-  a.clear();
-  a = "bgezal";
-  list.push_back(a);
-  a.clear();
-  a = "bgtz";
-  list.push_back(a);
-  a.clear();
+    a = "and";
+    list.push_back(a);
+    a.clear();
+    a = "andi";
+    list.push_back(a);
+    a.clear();
+    a = "beq";
+    list.push_back(a);
+    a.clear();
 
-  a = "blez";
-  list.push_back(a);
-  a.clear();
-  a = "bltz";
-  list.push_back(a);
-  a.clear();
-  a = "bltzal";
-  list.push_back(a);
-  a.clear();
+    a = "bgez";
+    list.push_back(a);
+    a.clear();
+    a = "bgezal";
+    list.push_back(a);
+    a.clear();
+    a = "bgtz";
+    list.push_back(a);
+    a.clear();
 
-  a = "bne";
-  list.push_back(a);
-  a.clear();
-  a = "div";
-  list.push_back(a);
-  a.clear();
-  a = "divu";
-  list.push_back(a);
-  a.clear();
+    a = "blez";
+    list.push_back(a);
+    a.clear();
+    a = "bltz";
+    list.push_back(a);
+    a.clear();
+    a = "bltzal";
+    list.push_back(a);
+    a.clear();
 
-  a = "j";
-  list.push_back(a);
-  a.clear();
-  a = "jal";
-  list.push_back(a);
-  a.clear();
-  a = "jr";
-  list.push_back(a);
-  a.clear();
+    a = "bne";
+    list.push_back(a);
+    a.clear();
+    a = "div";
+    list.push_back(a);
+    a.clear();
+    a = "divu";
+    list.push_back(a);
+    a.clear();
 
-  a = "lb";
-  list.push_back(a);
-  a.clear();
-  a = "lui";
-  list.push_back(a);
-  a.clear();
-  a = "lw";
-  list.push_back(a);
-  a.clear();
+    a = "j";
+    list.push_back(a);
+    a.clear();
+    a = "jal";
+    list.push_back(a);
+    a.clear();
+    a = "jr";
+    list.push_back(a);
+    a.clear();
 
-  a = "mfhi";
-  list.push_back(a);
-  a.clear();
-  a = "mflo";
-  list.push_back(a);
-  a.clear();
-  a = "mult";
-  list.push_back(a);
-  a.clear();
+    a = "lb";
+    list.push_back(a);
+    a.clear();
+    a = "lui";
+    list.push_back(a);
+    a.clear();
+    a = "lw";
+    list.push_back(a);
+    a.clear();
 
-  a = "multu";
-  list.push_back(a);
-  a.clear();
-  a = "noop";
-  list.push_back(a);
-  a.clear();
-  a = "or";
-  list.push_back(a);
-  a.clear();
+    a = "mfhi";
+    list.push_back(a);
+    a.clear();
+    a = "mflo";
+    list.push_back(a);
+    a.clear();
+    a = "mult";
+    list.push_back(a);
+    a.clear();
 
-  a = "ori";
-  list.push_back(a);
-  a.clear();
-  a = "sb";
-  list.push_back(a);
-  a.clear();
-  a = "sll";
-  list.push_back(a);
-  a.clear();
+    a = "multu";
+    list.push_back(a);
+    a.clear();
+    a = "noop";
+    list.push_back(a);
+    a.clear();
+    a = "or";
+    list.push_back(a);
+    a.clear();
 
-  a = "sllv";
-  list.push_back(a);
-  a.clear();
-  a = "slt";
-  list.push_back(a);
-  a.clear();
-  a = "slti";
-  list.push_back(a);
-  a.clear();
+    a = "ori";
+    list.push_back(a);
+    a.clear();
+    a = "sb";
+    list.push_back(a);
+    a.clear();
+    a = "sll";
+    list.push_back(a);
+    a.clear();
 
-  a = "sltiu";
-  list.push_back(a);
-  a.clear();
-  a = "sltu";
-  list.push_back(a);
-  a.clear();
-  a = "sra";
-  list.push_back(a);
-  a.clear();
+    a = "sllv";
+    list.push_back(a);
+    a.clear();
+    a = "slt";
+    list.push_back(a);
+    a.clear();
+    a = "slti";
+    list.push_back(a);
+    a.clear();
 
-  a = "srl";
-  list.push_back(a);
-  a.clear();
-  a = "srlv";
-  list.push_back(a);
-  a.clear();
-  a = "sub";
-  list.push_back(a);
-  a.clear();
+    a = "sltiu";
+    list.push_back(a);
+    a.clear();
+    a = "sltu";
+    list.push_back(a);
+    a.clear();
+    a = "sra";
+    list.push_back(a);
+    a.clear();
 
-  a = "subu";
-  list.push_back(a);
-  a.clear();
-  a = "sw";
-  list.push_back(a);
-  a.clear();
-  a = "syscall";
-  list.push_back(a);
-  a.clear();
+    a = "srl";
+    list.push_back(a);
+    a.clear();
+    a = "srlv";
+    list.push_back(a);
+    a.clear();
+    a = "sub";
+    list.push_back(a);
+    a.clear();
 
-  a = "xor";
-  list.push_back(a);
-  a.clear();
-  a = "xori";
-  list.push_back(a);
-  a.clear();
+    a = "subu";
+    list.push_back(a);
+    a.clear();
+    a = "sw";
+    list.push_back(a);
+    a.clear();
+    a = "syscall";
+    list.push_back(a);
+    a.clear();
+
+    a = "xor";
+    list.push_back(a);
+    a.clear();
+    a = "xori";
+    list.push_back(a);
+    a.clear();
+  }
+
+  std::vector < unsigned int > uints;
+  std::list < std::string > ::iterator it;
+  for (it = list.begin(); it != list.end(); ++it)
+  {
+    // std::cout << "hashing ";
+    // std::cout << *(it) << " ... ";
+    uints.push_back(alt_hash((*it)));
+  }
+
+  // push key (hash) and string (command) into map
+  std::map < unsigned int, std::string > map;
+  int i = 0;
+  for (it = list.begin(); it != list.end(); ++it)
+  {
+    map.insert ( std::pair < unsigned int, std::string > ( uints[i], *(it) ) );
+    ++i;
+  }
+  return map;
 }
 
+
+void print_map_commands(std::map < unsigned int, std::string> map)
+{
+  // print map:
+  std::map < unsigned int, std::string > ::iterator it2 = map.begin();
+  int j = 0;
+  for (it2 = map.begin(); it2 != map.end(); ++it2)
+  {
+    std::cout << j << ". ";
+    std::cout << it2->first << " => " << it2->second << '\n';
+    ++j;
+  }
+}
+
+
+void print_map_registers(std::map < unsigned int, unsigned int > map)
+{
+  // print map:
+  std::map < unsigned int, unsigned int > ::iterator it2 = map.begin();
+  int j = 0;
+  for (it2 = map.begin(); it2 != map.end(); ++it2)
+  {
+    std::cout << j << ". ";
+    std::cout << it2->first << " => " << it2->second << '\n';
+    ++j;
+  }
+}
 
 int main()
 {
   std::list < std::string > instructions;
   std::list < std::vector < unsigned int > > instructions_uint;
 
-  map_commands();
+  // creates and itializes command map - format: unsigned int hash, string command
+  std::map < unsigned int, std::string > map = map_commands();
+  
+  // creates and itializes registers map
+  std::map < unsigned int , unsigned int > map_registers;
+  create_registers(map_registers);
+  print_map_registers(map_registers);
   return 0;
   
   while(1)
@@ -429,8 +463,7 @@ int main()
 
       // tokenize() parses unsigned int vector
       //tokenize(uints);
-      std::map < int , unsigned int > map;
-      create_registers(map);
+      
       //print_registers(map);
       //print_instructions_uint(instructions_uint);
       interpret(uints);
