a1f5384b3ad1b0e108126cf1c5ac439f52979fb4
Christian Elliott <cdelliott96@gmail.com>
2019-12-11 20:41:27 -0600

    moved functions into Functions.h, building token class

--- assembly_interpreter/Interpreter.h
+++ assembly_interpreter/Interpreter.h
@@ -1,11 +1,39 @@
 #ifndef INTERPRETER_H
 #define INTERPRETER_H
 
-
+#include <iostream>
 #include <string>
+#include <vector>
+#include <list>
+#include <iterator>
+#include <map>
+#include <functional>
+#include "Functions.h"
 
 
-// first class: handles input string ??
+class tok
+{
+public:
+    std::string token;
+    bool is_command;
+    bool is_register;
+    bool is_constant;
+    // might need to add a "is_data" or similar member later
+    void determine_type();
+    tok()
+    {
+        is_command = false;
+        is_register = false;
+        is_constant = false;
+    }
+};
+
+
+void tok::determine_type()
+{
+    std::string::iterator it = token.begin();
+    
+}
 
 
 #endif
